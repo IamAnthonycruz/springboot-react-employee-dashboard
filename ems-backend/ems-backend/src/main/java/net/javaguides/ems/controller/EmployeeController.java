13a0f1eb0c844c7a49559cf136828029e4ff34c4
IamAnthonyCruz <Charliecruz1224@gmail.com>
2025-10-04 17:27:49 -0400

    Initial commit - React frontend + Spring Boot backend

---
+++ ems-backend/ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package net.javaguides.ems.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.service.EmployeeService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin("*")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+    //Build add employee rest api
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+    //build get employee rest api
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") long employeeId) {
+       EmployeeDto employeeDto =  employeeService.getEmployeeById(employeeId);
+       return ResponseEntity.ok(employeeDto);
+    }
+
+    //build get all employees rest api
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        List<EmployeeDto>employees =  employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+    //build update rest api
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId, @RequestBody EmployeeDto updatedEmployee) {
+      EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
+      return ResponseEntity.ok(employeeDto);
+    }
+    //Build delete employee rest api
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted successfully");
+
+    }
+}
